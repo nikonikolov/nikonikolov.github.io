295a85140eb544a55b3eae1f2de4d053f76a061e
nikonikolov <niko.b.nikolov@gmail.com>
2017-08-12 01:25:30 +0100

    transition to jekyll

--- footer/footer.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function () {	
-	//$("#navigation").load("/navbar/navbar.html");	
-	$("#footer").load("/footer/footer.html", function() {
-    	$.getScript("/footer/footerscript.js");
-	});	
-});
--- navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-$(document).ready( function () {	
-	//$("#navigation").load("/navbar/navbar.html");	
-	$("#navigation").load("/navbar/navbar.html", function() {
-    	$.getScript("/navbar/navbarscript.js");
-	});	
-});
